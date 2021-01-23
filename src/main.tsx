1213371595f52e82f9bf85e64a457d8552dc6106
Dan Nguyen <nghuudan@gmail.com>
2021-01-22 20:37:47 -0500

    Use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route exact path="/">
         <h1>Hello</h1>
@@ -12,7 +12,7 @@ const App = () => (
         <h1>About</h1>
       </Route>
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 render(<App />, document.getElementById('app'));
